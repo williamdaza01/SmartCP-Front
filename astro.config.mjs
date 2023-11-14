a9ba712eccdbd220a7aeb9887aced30206375fc6
William Daza <williamdaza04@gmail.com>
2023-11-14 00:15:48 -0500

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,22 @@
 import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: nodejs({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [tailwind(), react()],
-  
-    middleware: [
-      (req, res, next) => {
-        console.log("🚀 ~ file: astro.config.mjs:17 ~ req:", req)
-        const userCookie = req.cookies.id_user;
-  
-        if (!userCookie) {
-          res.redirect('/login');
-          return;
-        }
-  
-        next();
-      },
-    ],
-  
+  middleware: [(req, res, next) => {
+    console.log("🚀 ~ file: astro.config.mjs:17 ~ req:", req);
+    const userCookie = req.cookies.id_user;
+    if (!userCookie) {
+      res.redirect('/login');
+      return;
+    }
+    next();
+  }]
 });
